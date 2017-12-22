fa00cb205d092f69e08e44720814e535b72e967e
Karl Inglis <mail@karlinglis.net>
2017-12-22 19:48:59 +0000

    Do init in Org format

--- custom.el
+++ custom.el
@@ -3,20 +3,32 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#515151" "#f2777a" "#99cc99" "#ffcc66" "#6699cc" "#cc99cc" "#66cccc" "#cccccc"))
+ '(beacon-color "#f2777a")
  '(css-indent-offset 2)
- '(custom-enabled-themes (quote (zenburn)))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-bright)))
  '(custom-safe-themes
    (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default)))
+    ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default)))
+ '(fci-rule-color "#515151")
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
  '(inhibit-startup-screen t)
  '(js-indent-level 2)
+ '(line-number-mode t)
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(org-agenda-files
    (quote
     ("/home/karl/org/ef.org" "/home/karl/org/capture-mobile.org" "/home/karl/org/capture.org" "/home/karl/org/ds-list.org" "/home/karl/org/food.org" "/home/karl/org/from-mobile.org" "/home/karl/org/imagetest.org" "/home/karl/org/journal.org" "/home/karl/org/jujitsu.org" "/home/karl/org/notebook.org" "/home/karl/org/ou.org" "/home/karl/org/personal.org" "/home/karl/org/project-euler.org" "/home/karl/org/system.org" "/home/karl/org/projects/balletschoolband.com.org" "/home/karl/org/projects/blanqui.kingston.org" "/home/karl/org/projects/cahiers.org" "/home/karl/org/projects/christianscienceblackheath.org.uk.org" "/home/karl/org/projects/flynvibert.com.org" "/home/karl/org/projects/karlinglis.net.org" "/home/karl/org/projects/katrinehjelde.net.org" "/home/karl/org/projects/michaelchekhov.org.uk.org" "/home/karl/org/projects/nicolestandenmills.co.uk.org" "/home/karl/org/projects/radio-astronomy.org" "/home/karl/org/projects/royalgreenwichfencingclub.org.uk.org" "/home/karl/org/projects/rpsnw.com.org" "/home/karl/org/projects/sineadrushe.co.uk.org" "/home/karl/org/projects/tomthompsondesign.co.uk.org")))
  '(org-export-backends (quote (ascii html icalendar latex md odt)))
  '(package-selected-packages
    (quote
-    (scad-mode tide spacemacs-theme s markdown-mode typescript-mode projectile smart-mode-line ssass-mode scss-mode js2-refactor js2-mode ace-window geiser magit zenburn-theme web-mode rainbow-mode queue php-mode paredit org lorem-ipsum json-mode jinja2-mode jedi graphviz-dot-mode gnuplot-mode gnuplot glsl-mode flymake-jslint flymake-css flycheck crontab-mode buffer-move backtrace-mode apache-mode)))
+    (ivy flatland-theme color-theme-sanityinc-tomorrow scad-mode tide spacemacs-theme s markdown-mode typescript-mode projectile smart-mode-line ssass-mode scss-mode js2-refactor js2-mode ace-window geiser magit zenburn-theme web-mode rainbow-mode queue php-mode paredit org lorem-ipsum json-mode jinja2-mode jedi graphviz-dot-mode gnuplot-mode gnuplot glsl-mode flymake-jslint flymake-css flycheck crontab-mode buffer-move backtrace-mode apache-mode)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(reb-auto-match-limit 2000)
  '(rng-schema-locating-files
    (quote
@@ -31,7 +43,29 @@
  '(show-paren-mode t)
  '(standard-indent 2)
  '(tool-bar-mode nil)
- '(typescript-indent-level 2))
+ '(typescript-indent-level 2)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#f2777a")
+     (40 . "#f99157")
+     (60 . "#ffcc66")
+     (80 . "#99cc99")
+     (100 . "#66cccc")
+     (120 . "#6699cc")
+     (140 . "#cc99cc")
+     (160 . "#f2777a")
+     (180 . "#f99157")
+     (200 . "#ffcc66")
+     (220 . "#99cc99")
+     (240 . "#66cccc")
+     (260 . "#6699cc")
+     (280 . "#cc99cc")
+     (300 . "#f2777a")
+     (320 . "#f99157")
+     (340 . "#ffcc66")
+     (360 . "#99cc99"))))
+ '(vc-annotate-very-old-color nil))
 
 
 (custom-set-faces
