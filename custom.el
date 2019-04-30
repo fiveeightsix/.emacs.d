b410fd2687e8365b4b00d88ff7ecc26415c957a2
Karl Inglis <kinglis@tradeteq.com>
2019-04-30 10:01:46 +0100

    Remove scss-mode and zenburn theme

--- custom.el
+++ custom.el
@@ -3,19 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   (vector "#424242" "#d54e53" "#b9ca4a" "#e7c547" "#7aa6da" "#c397d8" "#70c0b1" "#eaeaea"))
- '(beacon-color "#d54e53")
  '(css-indent-offset 2)
- '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
  '(custom-safe-themes
    (quote
     ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default)))
- '(fci-rule-color "#424242")
- '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
- '(frame-background-mode (quote dark))
  '(inhibit-startup-screen t)
  '(js-indent-level 2)
  '(line-number-mode t)
@@ -28,7 +20,7 @@
  '(org-export-backends (quote (ascii html icalendar latex md odt)))
  '(package-selected-packages
    (quote
-    (editorconfig auto-complete org-tree-slide ox-gfm plantuml-mode rjsx-mode csv-mode diminish spaceline powerline counsel ivy flatland-theme color-theme-sanityinc-tomorrow scad-mode tide spacemacs-theme s markdown-mode typescript-mode projectile ssass-mode scss-mode js2-refactor js2-mode ace-window geiser magit zenburn-theme web-mode rainbow-mode queue php-mode paredit org lorem-ipsum json-mode jinja2-mode jedi graphviz-dot-mode gnuplot-mode gnuplot glsl-mode flymake-jslint flymake-css flycheck crontab-mode buffer-move backtrace-mode apache-mode)))
+    (editorconfig auto-complete org-tree-slide ox-gfm plantuml-mode rjsx-mode csv-mode diminish spaceline powerline counsel ivy flatland-theme color-theme-sanityinc-tomorrow scad-mode tide spacemacs-theme s markdown-mode typescript-mode projectile ssass-mode js2-refactor js2-mode ace-window geiser magit web-mode rainbow-mode queue php-mode paredit org lorem-ipsum json-mode jinja2-mode jedi graphviz-dot-mode gnuplot-mode gnuplot glsl-mode flymake-jslint flymake-css flycheck crontab-mode buffer-move backtrace-mode apache-mode)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(reb-auto-match-limit 2000)
  '(rng-schema-locating-files
@@ -42,29 +34,7 @@
      (projectile-project-compilation-cmd . "node-sass sass/style.scss > style.css"))))
  '(show-paren-mode t)
  '(standard-indent 2)
- '(typescript-indent-level 2)
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#d54e53")
-     (40 . "#e78c45")
-     (60 . "#e7c547")
-     (80 . "#b9ca4a")
-     (100 . "#70c0b1")
-     (120 . "#7aa6da")
-     (140 . "#c397d8")
-     (160 . "#d54e53")
-     (180 . "#e78c45")
-     (200 . "#e7c547")
-     (220 . "#b9ca4a")
-     (240 . "#70c0b1")
-     (260 . "#7aa6da")
-     (280 . "#c397d8")
-     (300 . "#d54e53")
-     (320 . "#e78c45")
-     (340 . "#e7c547")
-     (360 . "#b9ca4a"))))
- '(vc-annotate-very-old-color nil))
+ '(typescript-indent-level 2))
 
 
 (custom-set-faces
@@ -72,4 +42,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 143 :width normal)))))
+ )
