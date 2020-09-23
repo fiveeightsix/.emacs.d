d8cd98b2cfd25bd8b7072c2830b178529235ff0a
Karl Inglis <kinglis@tradeteq.com>
2020-09-23 19:52:54 +0100

    Fix default font setting

--- custom.el
+++ custom.el
@@ -4,34 +4,28 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(css-indent-offset 2)
- '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
+ '(custom-enabled-themes '(sanityinc-tomorrow-eighties))
  '(custom-safe-themes
-   (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default)))
+   '("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default))
  '(inhibit-startup-screen t)
  '(js-indent-level 2)
  '(line-number-mode t)
  '(nrepl-message-colors
-   (quote
-    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
  '(org-agenda-files
-   (quote
-    ("/home/karl/org/capture-mobile.org" "/home/karl/org/capture.org" "/home/karl/org/ds-list.org" "/home/karl/org/food.org" "/home/karl/org/from-mobile.org" "/home/karl/org/imagetest.org" "/home/karl/org/journal.org" "/home/karl/org/jujitsu.org" "/home/karl/org/notebook.org" "/home/karl/org/ou.org" "/home/karl/org/personal.org" "/home/karl/org/project-euler.org" "/home/karl/org/system.org" "/home/karl/org/xwing.org" "/home/karl/org/projects/balletschoolband.com.org" "/home/karl/org/projects/blanqui.kingston.org" "/home/karl/org/projects/cahiers.org" "/home/karl/org/projects/christianscienceblackheath.org.uk.org" "/home/karl/org/projects/flynvibert.com.org" "/home/karl/org/projects/karlinglis.net.org" "/home/karl/org/projects/katrinehjelde.net.org" "/home/karl/org/projects/michaelchekhov.org.uk.org" "/home/karl/org/projects/nicolestandenmills.co.uk.org" "/home/karl/org/projects/radio-astronomy.org" "/home/karl/org/projects/royalgreenwichfencingclub.org.uk.org" "/home/karl/org/projects/rpsnw.com.org" "/home/karl/org/projects/shapecast.org" "/home/karl/org/projects/sineadrushe.co.uk.org" "/home/karl/org/projects/tomthompsondesign.co.uk.org")))
- '(org-export-backends (quote (ascii html icalendar latex md odt)))
+   '("/home/karl/org/capture-mobile.org" "/home/karl/org/capture.org" "/home/karl/org/ds-list.org" "/home/karl/org/food.org" "/home/karl/org/from-mobile.org" "/home/karl/org/imagetest.org" "/home/karl/org/journal.org" "/home/karl/org/jujitsu.org" "/home/karl/org/notebook.org" "/home/karl/org/ou.org" "/home/karl/org/personal.org" "/home/karl/org/project-euler.org" "/home/karl/org/system.org" "/home/karl/org/xwing.org" "/home/karl/org/projects/balletschoolband.com.org" "/home/karl/org/projects/blanqui.kingston.org" "/home/karl/org/projects/cahiers.org" "/home/karl/org/projects/christianscienceblackheath.org.uk.org" "/home/karl/org/projects/flynvibert.com.org" "/home/karl/org/projects/karlinglis.net.org" "/home/karl/org/projects/katrinehjelde.net.org" "/home/karl/org/projects/michaelchekhov.org.uk.org" "/home/karl/org/projects/nicolestandenmills.co.uk.org" "/home/karl/org/projects/radio-astronomy.org" "/home/karl/org/projects/royalgreenwichfencingclub.org.uk.org" "/home/karl/org/projects/rpsnw.com.org" "/home/karl/org/projects/shapecast.org" "/home/karl/org/projects/sineadrushe.co.uk.org" "/home/karl/org/projects/tomthompsondesign.co.uk.org"))
+ '(org-export-backends '(ascii html icalendar latex md odt))
  '(package-selected-packages
-   (quote
-    (powershell company lsp-mode csharp-mode yaml-mode web-mode fsharp-mode ripgrep cargo delight rust-mode toml-mode org-present editorconfig auto-complete org-tree-slide ox-gfm plantuml-mode rjsx-mode csv-mode diminish spaceline powerline counsel ivy flatland-theme color-theme-sanityinc-tomorrow scad-mode tide spacemacs-theme s markdown-mode typescript-mode projectile ssass-mode js2-refactor js2-mode ace-window geiser magit queue php-mode paredit org lorem-ipsum json-mode jinja2-mode jedi graphviz-dot-mode gnuplot-mode gnuplot glsl-mode flymake-jslint flymake-css flycheck crontab-mode buffer-move backtrace-mode apache-mode)))
- '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+   '(sql-indent powershell company lsp-mode csharp-mode yaml-mode web-mode fsharp-mode ripgrep cargo delight rust-mode toml-mode org-present editorconfig auto-complete org-tree-slide ox-gfm plantuml-mode rjsx-mode csv-mode diminish spaceline powerline counsel ivy flatland-theme color-theme-sanityinc-tomorrow scad-mode tide spacemacs-theme s markdown-mode typescript-mode projectile ssass-mode js2-refactor js2-mode ace-window geiser magit queue php-mode paredit org lorem-ipsum json-mode jinja2-mode jedi graphviz-dot-mode gnuplot-mode gnuplot glsl-mode flymake-jslint flymake-css flycheck crontab-mode buffer-move backtrace-mode apache-mode))
+ '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
  '(reb-auto-match-limit 2000)
  '(rng-schema-locating-files
-   (quote
-    ("schemas.xml" "~/emacs.d/schemas.xml" "c:/Program Files/emacs-23.2/etc/schema/schemas.xml")))
+   '("schemas.xml" "~/emacs.d/schemas.xml" "c:/Program Files/emacs-23.2/etc/schema/schemas.xml"))
  '(safe-local-variable-values
-   (quote
-    ((create-lockfiles)
+   '((create-lockfiles)
      (css-indent-offset . 4)
      (css-indent-offset . 8)
-     (projectile-project-compilation-cmd . "node-sass sass/style.scss > style.css"))))
+     (projectile-project-compilation-cmd . "node-sass sass/style.scss > style.css")))
  '(show-paren-mode t)
  '(standard-indent 2)
  '(typescript-indent-level 2))
