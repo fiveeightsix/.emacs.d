d69df6a8ab6054601b9c3b77ed8cae86873ad93a
Karl Inglis <kinglis@tradeteq.com>
2023-06-12 15:57:21 +0100

    Org present

--- custom.el
+++ custom.el
@@ -8,6 +8,7 @@
  '(custom-safe-themes
    '("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default))
  '(inhibit-startup-screen t)
+ '(ispell-dictionary nil)
  '(js-indent-level 2)
  '(line-number-mode t)
  '(nrepl-message-colors
@@ -22,7 +23,9 @@
  '(rng-schema-locating-files
    '("schemas.xml" "~/emacs.d/schemas.xml" "c:/Program Files/emacs-23.2/etc/schema/schemas.xml"))
  '(safe-local-variable-values
-   '((create-lockfiles)
+   '((org-image-actual-width . 600)
+     (org-image-actual-width)
+     (create-lockfiles)
      (css-indent-offset . 4)
      (css-indent-offset . 8)
      (projectile-project-compilation-cmd . "node-sass sass/style.scss > style.css")))
